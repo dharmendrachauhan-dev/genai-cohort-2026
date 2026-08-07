709ad8d5be0aed1b76d63ac171c32ac9dcc047c2
Dharmendra Chauhan <dmma359800@gmail.com>
2026-08-07 16:24:14 +0530

    feat: add ensureCollection function to manage Qdrant collection

--- srt-vrt-project/frontend/lib/qdrant.ts
+++ srt-vrt-project/frontend/lib/qdrant.ts
@@ -1,6 +1,24 @@
 import { QdrantClient } from "@qdrant/js-client-rest";
 
-export const qdrant = new QdrantClient({
+export const client = new QdrantClient({
   url: process.env.QDRANT_URL,
+  apiKey: process.env.QDRANT_API_KEY
 });
 
+export const COLLECTION_NAME= "subtitles"
+
+export async function ensureCollection(){
+  try {
+    await client.getCollection(COLLECTION_NAME);
+    console.log("Collection already exists")
+  } catch (error) {
+    await client.createCollection(COLLECTION_NAME, {
+      vectors: {
+        size: 1536,
+        distance: "Cosine"
+      }
+    })
+  }
+
+  console.log("✅ Collection created ")
+}
